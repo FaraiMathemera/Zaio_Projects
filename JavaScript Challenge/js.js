fff4343991bd4856ad4ae807d802304d9ac34a72
Farai Mathemera <mathemerafarai@gmail.com>
2019-07-03 11:19:12 +0200

    Update js.js

--- JavaScript Challenge/js.js
+++ JavaScript Challenge/js.js
@@ -1,15 +1,41 @@
 function myFunctionRed() {
+	
+  var string = "Red";
+  var total =0;
+	for (var i = 0; i < string.length; i++) {
+	total=total+(string.charCodeAt(i));	
+	}
+	
   mycolor4.style.backgroundColor = "red";
-  document.getElementById("mycolor4").value = "hi, my name is red";
+  document.getElementById("output1").innerHTML = "";
+  document.getElementById("output2").innerHTML = "&lt;hi, my name is red&gt;";
+  document.getElementById("output3").innerHTML = "&lt;"+ total +"&gt;";
 }
 
 
 function myFunctionBlue() {
+	
+	var string = "Blue";
+  var total =0;
+	for (var i = 0; i < string.length; i++) {
+	total=total+(string.charCodeAt(i));	
+	}
   mycolor4.style.backgroundColor = "blue";
- document.getElementById("mycolor4").value = "hi, my name is blue";
+  document.getElementById("output1").innerHTML = "";
+ document.getElementById("output2").innerHTML = "&lt; hi, my name is blue&gt;";
+ document.getElementById("output3").innerHTML = "&lt;"+ total +"&gt;";
 }
 
 function myFunctionGreen() {
+	
+	var string = "Green";
+  var total =0;
+	for (var i = 0; i < string.length; i++) {
+	total=total+(string.charCodeAt(i));	
+	}
+	
   mycolor4.style.backgroundColor = "green";
-  document.getElementById("mycolor4").value = "hi, my name is green";
+  document.getElementById("output1").innerHTML = "";
+  document.getElementById("output2").innerHTML = "&lt;hi, my name is green&gt;";
+  document.getElementById("output3").innerHTML = "&lt;"+ total +"&gt;";
 }
