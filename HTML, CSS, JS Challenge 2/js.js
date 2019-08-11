873f2b8573aab3efb2323aa9ca6b8e92aca1580d
Farai Mathemera <mathemerafarai@gmail.com>
2019-08-11 22:21:01 +0200

    Update js.js

--- HTML, CSS, JS Challenge 2/js.js
+++ HTML, CSS, JS Challenge 2/js.js
@@ -1,38 +1,64 @@
-//Functions to change colour label
+//********************Start of colour picker ************************//
 function myFunction1() {
   document.getElementById("colour").innerHTML = "Arctic White";
+  document.getElementById("modalcolour").innerHTML = "Arctic White";
   }
 function myFunction2() {
   document.getElementById("colour").innerHTML = "Honey Dew";
+  document.getElementById("modalcolour").innerHTML = "Honey Dew";
   }
 function myFunction3() {
   document.getElementById("colour").innerHTML = "Coral";
+  document.getElementById("modalcolour").innerHTML = "Coral";
   }
 function myFunction4() {
   document.getElementById("colour").innerHTML = "Salmon";
+  document.getElementById("modalcolour").innerHTML = "Salmon";
   }
 function myFunction5() {
   document.getElementById("colour").innerHTML = "Fuschia";
+  document.getElementById("modalcolour").innerHTML = "Fuschia";
   }
 function myFunction6() {
   document.getElementById("colour").innerHTML = "Spring Green";
+  document.getElementById("modalcolour").innerHTML = "Spring Green";
   }
 function myFunction7() {
   document.getElementById("colour").innerHTML = "Rosy Brown";
+  document.getElementById("modalcolour").innerHTML = "Rosy Brown";
   }
 function myFunction8() {
   document.getElementById("colour").innerHTML = "Silver";
+  document.getElementById("modalcolour").innerHTML = "Silver";
   }
 function myFunction9() {
   document.getElementById("colour").innerHTML = "Peru";
+  document.getElementById("modalcolour").innerHTML = "Peru";
   }
 function myFunction10() {
   document.getElementById("colour").innerHTML = "Sienna";
+  document.getElementById("modalcolour").innerHTML = "Sienna";
   }
 function myFunction11() {
   document.getElementById("colour").innerHTML = "Navy";
+  document.getElementById("modalcolour").innerHTML = "Navy";
   }
 function myFunction12() {
   document.getElementById("colour").innerHTML = "Black";
+  document.getElementById("modalcolour").innerHTML = "Black";
+  }
+//********************End of colour picker ************************//  
+
+
+//Global variables for cart quantity
+var quantity = 0;
+//Function to increase number in cart
+function myFunctionadd() {
+  quantity = quantity + 1;
+  document.getElementById("quantity").innerHTML = "Sienna";
+  }
+//Function to decrease number in cart
+function myFunctionminus() {
+  quantity = quantity - 1;
   }
   
