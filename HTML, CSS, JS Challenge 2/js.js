b65216394a3972cb1863e8d5b1c2d6ffe8d513b2
Farai Mathemera <mathemerafarai@gmail.com>
2019-08-11 21:10:13 +0200

    Create js.js

---
+++ HTML, CSS, JS Challenge 2/js.js
@@ -0,0 +1,38 @@
+//Functions to change colour label
+function myFunction1() {
+  document.getElementById("colour").innerHTML = "Arctic White";
+  }
+function myFunction2() {
+  document.getElementById("colour").innerHTML = "Honey Dew";
+  }
+function myFunction3() {
+  document.getElementById("colour").innerHTML = "Coral";
+  }
+function myFunction4() {
+  document.getElementById("colour").innerHTML = "Salmon";
+  }
+function myFunction5() {
+  document.getElementById("colour").innerHTML = "Fuschia";
+  }
+function myFunction6() {
+  document.getElementById("colour").innerHTML = "Spring Green";
+  }
+function myFunction7() {
+  document.getElementById("colour").innerHTML = "Rosy Brown";
+  }
+function myFunction8() {
+  document.getElementById("colour").innerHTML = "Silver";
+  }
+function myFunction9() {
+  document.getElementById("colour").innerHTML = "Peru";
+  }
+function myFunction10() {
+  document.getElementById("colour").innerHTML = "Sienna";
+  }
+function myFunction11() {
+  document.getElementById("colour").innerHTML = "Navy";
+  }
+function myFunction12() {
+  document.getElementById("colour").innerHTML = "Black";
+  }
+  
